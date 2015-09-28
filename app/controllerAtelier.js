28fa687a382ecf87f5bf5017e9875f7d9702ae44
nureynisow <nureynisow@github.com>
2015-09-28 16:28:29 +0200

    show data

--- app/controllerAtelier.js
+++ app/controllerAtelier.js
@@ -2,11 +2,26 @@ var x = new XMLHttpRequest();
 x.open('GET','http://localhost:4567/atelier',true);
 x.onreadystatechange = function () {
           if(x.readyState==4 && x.status == "200")
-            fillForm(JSON.parse(x.responseText));
+			fillForm(JSON.parse(x.responseText));
         }
 x.send();
 
 function fillForm (json) {
-	console.log (json);
-	document.getElementById('title_span').innerHtml = json.titre;
+	json = json[0];
+	console.log (json['title']);
+	for (key in json)
+
+		if(key=='date'  || key=='public'){
+			for (k in json[key]){
+				console.log(json[key])
+				document.getElementById(k).checked = json[key][k]
+			}
+		}else{
+			if(key=='free')
+				document.getElementById(key).checked = json[key]
+			else{
+				console.log(json[key]);
+				document.getElementById(key).value = json[key]
+			}
+		}
 }
