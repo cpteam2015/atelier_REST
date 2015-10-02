63c3bb19030467c935d8c1308323fb6dcd3d3cbc
nla15 <misstynet@hotmail.fr>
2015-10-02 13:43:18 +0200

    update controleListeAtelier.js

--- public/js/controleListeAtelier.js
+++ public/js/controleListeAtelier.js
@@ -22,8 +22,8 @@ function chargement (json) {
 			var ligne = document.createElement('tr');
 			ligne.innerHTML = '<td><input type="text" id ="idTitre_'+ i +'" name="Nom_Atelier" disabled=True style="width:97%;"><td><input type="text" id ="idTheme_'+ i +'" name="Theme" disabled=True style="width:97%;"></td><td><input type="button" id="visual_'+i+'" onclick=window.open("atelier.html") value="visualiser" style="margin:0 10px;" /><input type="button" id="sup_'+i+'" onclick="supprimer('+i+');" value="supprimer" /></td></tr>'
 			document.getElementById('listAt').appendChild(ligne);
-			tabTitre[j] = console.log (json['title']);
-			tabTheme[j] = console.log (json['theme']);
+			tabTitre[j] = console.log (Json['title']);
+			tabTheme[j] = console.log (Json['theme']);
 			document.getElementById('idTitre_'+i+'').value = tabTitre[j];
 			document.getElementById('idTheme_'+i+'').value = tabTheme[j];
 			j=j+1;
