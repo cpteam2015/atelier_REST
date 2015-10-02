e21d559cc90af1c5dc92da36975f8606bc5b1690
nla15 <misstynet@hotmail.fr>
2015-10-02 14:02:16 +0200

    update controleListeAtelier.js

--- public/js/controleListeAtelier.js
+++ public/js/controleListeAtelier.js
@@ -1,8 +1,8 @@
 var data = new XMLHttpRequest();
 data.open('GET','mockup.json',true);
 data.onreadystatechange = function () {
-          if(x.readyState==4 && x.status == "200")
-			chargement(JSON.parse(x.responseText));
+          if(data.readyState==4 && data.status == "200")
+			chargement(JSON.parse(data.responseText));
 		  else
 			dump("erreur pendant le chargement de la page.\n");
         }
